001cc804db165420a1fa85cdd906a2c43abfff5a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-10 20:31:10 +0900

    taskモデル更新

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,15 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
+  before_validation :set_nameless_name
 
   private
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
