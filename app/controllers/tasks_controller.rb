5ad45b81d054bec481b2aeda923c2f9375dd7d97
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-06-03 14:17:15 +0900

    フィルタを使い重複を避ける

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
     # @task = Task.find(params[:id])
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -24,24 +25,27 @@ class TasksController < ApplicationController
 
   def edit
     # @task = Task.find(params[:id])
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
     # task = Task.find(params[:id])
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    # task = current_user.tasks.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
     # task = Task.find(params[:id])
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    # task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :description)
