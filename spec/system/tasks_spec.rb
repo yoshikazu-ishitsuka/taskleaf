94159bd73c443c3b42b06f19a34a0868b40afe5e
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-06-13 10:20:22 +0900

    letを利用した共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,23 +2,27 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
       # ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       # 作成者がユーザーAであるタスクを作成しておく
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
 
         # ユーザーAでログインする
         visit login_path
         # メールアドレスを入力する
-        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'メールアドレス', with: login_user.email
         # パスワードを入力する
-        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード', with: login_user.password
         # 「ログインする」ボタンを押す
         click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
       # before do
       # end
 
@@ -28,15 +32,16 @@ describe 'タスク管理機能', type: :system do
       end
 
       context 'ユーザーBがログインしているとき' do
-        before do
+        let(:login_user) { user_b }
+        # before do
           # ユーザーBを作成しておく
-          FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+          # FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
         #   # ユーザーBでログインする
         #   visit login_path
         #   fill_in 'メールアドレス', with: 'b@example.com'
         #   fill_in 'パスワード', with: 'password'
         #   click_button 'ログインする'
-        end
+        # end
 
         it 'ユーザーAが作成したタスクが表示されない' do
           # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
