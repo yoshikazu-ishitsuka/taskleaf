32cb090f9eeb7bd18d15831f5a7a82dbf4c9347a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-06-19 19:14:15 +0900

    デバッグ用にタスク作成時にタスクの情報をログ出力する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new(task_params)
     if @task.save
       # task.save!
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
